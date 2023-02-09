a762931fd9ac4f357fbcb8beb820f03adc10d520
hunter-cloud09 <393009865@qq.com>
2023-02-09 13:56:53 +0800

    feat(visualis-server): update linkis version to 1.3.2

--- server/src/main/scala/com/webank/wedatasphere/dss/visualis/utils/VisualisSpringConfiguration.scala
+++ server/src/main/scala/com/webank/wedatasphere/dss/visualis/utils/VisualisSpringConfiguration.scala
@@ -1,21 +1,14 @@
 package com.webank.wedatasphere.dss.visualis.utils
 
-import com.webank.wedatasphere.dss.visualis.entrance.spark.{VisualisCSEntranceInterceptor, VisualisEntranceParser}
-import org.apache.linkis.entrance.EntranceParser
-import org.apache.linkis.entrance.annotation._
+import com.webank.wedatasphere.dss.visualis.entrance.spark.VisualisCSEntranceInterceptor
 import org.apache.linkis.entrance.conf.EntranceConfiguration.ENTRANCE_SCHEDULER_MAX_PARALLELISM_USERS
-import org.apache.linkis.entrance.execute.impl.EntranceExecutorManagerImpl
-import org.apache.linkis.entrance.execute._
 import org.apache.linkis.entrance.interceptor.EntranceInterceptor
 import org.apache.linkis.entrance.interceptor.impl._
 import org.apache.linkis.entrance.persistence.PersistenceManager
 import org.apache.linkis.entrance.scheduler.EntranceSchedulerContext
 import org.apache.linkis.entrance.scheduler.cache.ReadCacheConsumerManager
-import org.apache.linkis.scheduler.SchedulerContext
 import org.apache.linkis.scheduler.executer.ExecutorManager
 import org.apache.linkis.scheduler.queue.{ConsumerManager, GroupFactory}
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.{Bean, Configuration, Primary}
 
 
@@ -23,13 +16,13 @@ import org.springframework.context.annotation.{Bean, Configuration, Primary}
 class VisualisSpringConfiguration {
 
   @Primary
-  @EntranceInterceptorBeanAnnotation
-  def generateEntranceInterceptors: Array[EntranceInterceptor] = Array[EntranceInterceptor](new VisualisCSEntranceInterceptor, new ShellDangerousGrammerInterceptor, new PythonCodeCheckInterceptor, new DBInfoCompleteInterceptor, new SparkCodeCheckInterceptor, new SQLCodeCheckInterceptor, new VarSubstitutionInterceptor, new LogPathCreateInterceptor, new StorePathEntranceInterceptor, new ScalaCodeInterceptor, new SQLLimitEntranceInterceptor, new CommentInterceptor, new PythonCodeCheckInterceptor)
+  @Bean
+  def entranceInterceptors: Array[EntranceInterceptor] = Array[EntranceInterceptor](new VisualisCSEntranceInterceptor, new ShellDangerousGrammerInterceptor, new PythonCodeCheckInterceptor, new DBInfoCompleteInterceptor, new SparkCodeCheckInterceptor, new SQLCodeCheckInterceptor, new VarSubstitutionInterceptor, new LogPathCreateInterceptor, new StorePathEntranceInterceptor, new ScalaCodeInterceptor, new SQLLimitEntranceInterceptor, new CommentInterceptor, new PythonCodeCheckInterceptor)
 
   @Primary
-  @ConsumerManagerBeanAnnotation
-  def generateConsumerManager(@PersistenceManagerBeanAnnotation.PersistenceManagerAutowiredAnnotation persistenceManager: PersistenceManager) = new ReadCacheConsumerManager(ENTRANCE_SCHEDULER_MAX_PARALLELISM_USERS.getValue, persistenceManager)
+  @Bean
+  def generateConsumerManager(persistenceManager: PersistenceManager) = new ReadCacheConsumerManager(ENTRANCE_SCHEDULER_MAX_PARALLELISM_USERS.getValue, persistenceManager)
 
-  @SchedulerContextBeanAnnotation
-  def generateSchedulerContext(@GroupFactoryBeanAnnotation.GroupFactoryAutowiredAnnotation groupFactory: GroupFactory, @EntranceExecutorManagerBeanAnnotation.EntranceExecutorManagerAutowiredAnnotation executorManager: ExecutorManager, @ConsumerManagerBeanAnnotation.ConsumerManagerAutowiredAnnotation consumerManager: ConsumerManager) = new EntranceSchedulerContext(groupFactory, consumerManager, executorManager)
+  @Bean
+  def generateSchedulerContext(groupFactory: GroupFactory, executorManager: ExecutorManager, consumerManager: ConsumerManager) = new EntranceSchedulerContext(groupFactory, consumerManager, executorManager)
 }
